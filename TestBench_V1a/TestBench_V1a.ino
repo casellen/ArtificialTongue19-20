8dcc25cc453f9760f683bb5b1e15edec4a4b3ca5
casellen <36135978+casellen@users.noreply.github.com>
2020-02-24 23:10:06 -0500

    Pressure Sensor readouts
    unable to push on git desktop

--- TestBench_V1a/TestBench_V1a.ino
+++ TestBench_V1a/TestBench_V1a.ino
@@ -1,6 +1,6 @@
 /* Test Bench code for Artificial Tongue - V1a
- *  Basic timed inflation code to test seal on tongue
- *  Basic LCD display
+ *  Basic timed inflation code to test seal on tongue/misc things
+ *  Loop reads pressure sensor 1 and prints value to LCD!
  */
 // **** LIBRARIES TO INCLUDE ****
 #include <Wire.h>
@@ -8,7 +8,8 @@
 #include <LiquidCrystal_I2C.h>
 
 // ****** CONSTANTS AND VARIABLES ******
-//    PINOUTS
+
+//    PINOUTS - see pinout diagram in Claire's daily updates
 const int backSol = 10;     //Back Section D10
 const int midSol = 11;      // Middle section D11
 const int frontSol = 12;    // Front section D12
@@ -16,9 +17,14 @@ const int btn1 = 2;         // Button 1, D2
 const int btn2 = 3;         // Button 2, D3
 const int btn3 = 4;         // Button 3, D4
 const int btn4 = 5;         // Button 4, D5
+const int pres_1 = 5;       // pressure sensor 1 (white wire), A3
+const int pres_2 = 4;       // pressure sensor 2 (yellow wire), A2
+const int pres_3 = 3;       // pressure sensor 3 (black wire), A1
 
 LiquidCrystal_I2C  lcd(0x27,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified backpack
 
+// OTHER MISC CONSTANTS
+int Pmin = 0;   // minimum pressure reading on sensor
 //    VARIABLES FOR READINGS
 int frontBtn = 0;     // Button 1 - inflate front
 int midBtn = 0;       // Button 2 - inflate mid
@@ -41,6 +47,14 @@ void setup() {
   lcd.begin (20,4);          // for 20 x 4 LCD module
   lcd.setBacklightPin(3,POSITIVE);
   lcd.setBacklight(HIGH);
+
+  // set up pressure sensors
+  pinMode(pres_1, INPUT);     // Pressure sensor 1
+  pinMode(pres_2, INPUT);     // Pressure sensor 2
+  pinMode(pres_3, INPUT);     // Pressure sensor 3
+
+  // start serial monitor
+   Serial.begin(9600);     // print pressure values to serial read out, 9600 baud rate
 }
 // ****** HELPER FUNCTIONS ******
 void lcdMain() {
@@ -80,11 +94,32 @@ void inflateAll(int sec) {
    digitalWrite(frontSol, LOW);
 }
 
+float readPressure(int pin) {
+  /*
+   * takes in pressure sensor pin, 
+   * returns output pressure 
+   * modified from soft robotics toolkit
+   */
+   int sensorValue = analogRead(pin);       // reads sensor value
+   
+   // convert sensor value to voltage
+   float voltage = sensorValue * (5.0 / 1023.0);
+
+   // convert volts to PSI
+   float pressure_Value = ((voltage - (0.10 * 5.0)) / (0.80 * 5.0)) * 30;
+   
+   // return PSI value
+   return pressure_Value;
+   }
+
 // ******** MAIN LOOP *******
 void loop() {
   //lcdMain();      // Turn on LCD main menu
 
-  inflateAll(5);
+  inflateAll(2);
+  float pressure_1 = readPressure(pres_1);
+  lcd.print(pressure_1);
+  
   
 
 }
